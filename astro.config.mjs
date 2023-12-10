477161d2d3d98bc517c9078e062a34489c2ce061
Gabry <gabrielemleone@gmail.com>
2023-12-10 20:32:42 +0100

    fix: ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
             enabled: true
         }
     }),
-    output: 'hybrid',
+    output: 'server',
     integrations: [tailwind(), svelte()],
     publicDir: './static'
 })
