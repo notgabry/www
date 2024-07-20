8c51f15ab5ae2f8ef400c597d3fe012dddcf1482
Gabry <74418125+notgabry@users.noreply.github.com>
2024-07-20 19:48:40 +0200

    fix: removing fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,24 +13,7 @@ module.exports = {
                     'accent-2': '#262626',
                     'accent-3': '#204eeb'
                 }
-            },
-            boxShadow: {
-                neobrutalist: '8px 8px 0px rgba(0,0,0,1)',
-                'neobrutalist-low': '4px 4px 0px rgba(0,0,0,1)'
-            },
-            keyframes: {
-                shadowPulse: {
-                    '0%': { boxShadow: '0 0 0 0 var(--shadow-color)' },
-                    '80%': { boxShadow: '0 0 0 8px var(--shadow-color)44' },
-                    '100%': { boxShadow: '0 0 0 8px var(--shadow-color)00' }
-                }
-            },
-            animation: {
-                shadowPulse: 'shadowPulse 2s infinite'
             }
-        },
-        fontFamily: {
-            grotesque: ['Bricolage Grotesque', 'sans-serif']
         }
     },
     plugins: []
