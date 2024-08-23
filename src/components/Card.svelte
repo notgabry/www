7efcf26df70b3b729ce2be3018ffffab08bbfaa5
Gabry <74418125+notgabry@users.noreply.github.com>
2024-08-23 14:48:51 +0200

    feat: restyle, switching back to minimal

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
-    export let title
+    export let title: string
     export let sub = ''
-    export let url
-    export let image
-    export let plays = ''
+    export let url: string
+    export let image: string
+    export let small = ''
 
     let isLoading = true
 </script>
 
-<div class={`group h-20 block px-2 py-[0.55rem] ring-1 ring-neutral-800 bg-neutral-850 rounded-md truncate`}>
+<div class="group h-20 block px-2 py-[0.55rem] ring-1 ring-neutral-700/80 bg-neutral-850 rounded-md truncate">
     <div class="flex">
         <div class="max-w-[4rem] flex flex-shrink-0 h-full w-full">
             {#if isLoading}
@@ -31,9 +31,10 @@
             {/if}
         </div>
 
-        {#if plays}
-            <div class="self-center text-sm ring-1 ring-blue-600/40 text-blue-600 bg-blue-600/10 ml-auto whitespace-nowrap mx-1 px-2 py-1 rounded-md">
-                {plays} Plays
+        {#if small}
+            <div
+                class="self-center text-sm ring-1 ring-purple-600/40 text-purple-600 bg-purple-600/10 ml-auto whitespace-nowrap mx-1 px-2 py-1 rounded-md">
+                {small}
             </div>
         {/if}
     </div>
