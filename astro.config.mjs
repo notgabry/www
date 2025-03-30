e13f24477d7760cc74ed39c4c9f553dd3e426866
Gabry <gabrielemleone@gmail.com>
2025-03-30 11:21:34 +0200

    fix: analytics, serverless adapter, public dir

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,16 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import svelte from '@astrojs/svelte'
 import icon from 'astro-icon'
-import vercel from '@astrojs/vercel'
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind(), svelte(), icon()],
-    adapter: vercel(),
-    output: 'server'
+    adapter: vercel({
+        webAnalytics: {
+            enabled: true
+        }
+    }),
+    output: 'hybrid',
+    publicDir: './static'
 })
