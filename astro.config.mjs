7a9e6f851cad2eec8f172949cf196aae9a29a1ba
Gabry <gabrielemleone@gmail.com>
2025-03-30 11:15:16 +0200

    feat: adding adapter:

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config'
-
 import tailwind from '@astrojs/tailwind'
-
-import svelte from '@astrojs/svelte';
-
-import icon from 'astro-icon';
+import svelte from '@astrojs/svelte'
+import icon from 'astro-icon'
+import vercel from '@astrojs/vercel'
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), svelte(), icon()]
+    integrations: [tailwind(), svelte(), icon()],
+    adapter: vercel(),
+    output: 'server'
 })
